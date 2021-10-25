f4b99e35eb343f94a91fd396659fb26261daebed
Justin Stout <midwatch@jstout.us>
2021-10-25 14:43:12 -0700

    add create_tree

--- sphinx_notebook/__init__.py
+++ sphinx_notebook/__init__.py
@@ -1,5 +1,40 @@
 """Top-level package for Sphinx Notebook."""
 
+from pathlib import Path
+
+import anytree
+
+
 __author__ = """Justin Stout"""
 __email__ = 'midwatch@jstout.us'
 __version__ = '0.1.0'
+
+
+def _render_tree(root):
+    for pre, _, node in anytree.RenderTree(root):
+        print(f'{pre}{node.name}')
+
+
+def create_tree(src_dir):
+    """
+    src_dir = Path()
+    """
+    root = anytree.Node('root')
+    parent = root
+
+    src_dir = Path('tests/fixtures/notes')
+    notes = [path.relative_to(src_dir) for path in src_dir.glob('**/*.rst')]
+    notes.sort()
+
+    for note in notes:
+        for part in note.parts:
+
+            if not anytree.find_by_attr(root, part):
+                parent = anytree.Node(part, parent=parent)
+
+            else:
+                parent = anytree.find_by_attr(root, part)
+
+        parent = root
+
+    _render_tree(root)
--- sphinx_notebook/cli.py
+++ sphinx_notebook/cli.py
@@ -1,16 +1,23 @@
 """Console script for Sphinx Notebook."""
 
 import sys
+from pathlib import Path
 
 import click
 
+import sphinx_notebook
+
+SRC_DIR = Path('tests/fixtures/notes')
 
 @click.command()
 def main():
     """Console script for sphinx_notebook."""
-    click.echo("Replace this message by putting your code into "
-               "sphinx_notebook.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    # click.echo("Replace this message by putting your code into "
+    #            "sphinx_notebook.cli.main")
+    # click.echo("See click documentation at https://click.palletsprojects.com/")
+
+    sphinx_notebook.create_tree(SRC_DIR)
+
     return 0
 
 

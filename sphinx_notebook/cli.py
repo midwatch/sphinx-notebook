67ab254c9989b75f85d674edcebc4fd9bf2fe53e
Justin Stout <midwatch@jstout.us>
2021-10-28 12:05:39 -0700

    add custom template dir

--- sphinx_notebook/cli.py
+++ sphinx_notebook/cli.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 
 import click
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
 
 from sphinx_notebook import notebook
 
@@ -15,21 +15,25 @@ ENV = Environment(loader=PackageLoader("sphinx_notebook"),
 
 
 @click.command()
+@click.option('--template-dir', default=None, help="path to custom templates")
 @click.argument('src')
 @click.argument('dst')
-def main(src, dst):
+def main(template_dir, src, dst):
     """Render an index.rst file for a sphinx based notebook.
 
     SRC: path to source directory (eg notebook/)
 
     DST: path to index.rst (eg build/src/index.rst)
     """
+    if template_dir:
+        template_index = ENV.loader = FileSystemLoader(template_dir)
+
     root_dir = Path(src)
     output = Path(dst)
     root = notebook.get_tree(root_dir)
 
     with output.open(encoding='utf-8', mode='w') as out:
-        notebook.render_index(root, ENV.get_template("index.rst"), out)
+        notebook.render_index(root, ENV.get_template("index.rst.jinja"), out)
 
     return 0
 

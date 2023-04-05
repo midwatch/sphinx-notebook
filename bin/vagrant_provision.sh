0a4690667731fa9482df03e7ad4b85837d5a56bf
Justin Stout <midwatch@jstout.us>
2023-04-05 15:58:52 -0700

    update github config

--- bin/vagrant_provision.sh
+++ bin/vagrant_provision.sh
@@ -3,9 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Set Time Zone"
 timedatectl set-timezone America/Los_Angeles
 
-echo "Set Env Vars"
-ln -sf /home/vagrant/config/pypi-token.sh /etc/profile.d/pypi-token.sh
-
 echo "Add Ubuntu Dependencies"
 apt-get update
 apt-get upgrade -y
